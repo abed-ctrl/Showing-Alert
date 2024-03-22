2e282587a615e8daef600ce09a2b690019b34f72
Abdelali OULD DAOUIA <oprogrammerz@gmail.com>
2024-03-22 21:10:43 +0000

    Final Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
-  const handleClick = () => console.log("Clicked");
+  const handleOnClick = () => {
+    setAlertVisibility(true);
+  };
   const color = "primary";
   const icon = "bi-airplane-engines-fill";
 
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  const handleOnClose = () => {
+    setAlertVisibility(false);
+  };
+
   return (
     <div>
-      <Button onClick={handleClick} color={color} icon={icon}>
+      {alertVisible && <Alert onClose={handleOnClose}>Notification</Alert>}
+      <Button onClick={handleOnClick} color={color} icon={icon}>
         Click Me
       </Button>
     </div>
